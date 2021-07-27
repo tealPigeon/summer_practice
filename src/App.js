4dcf0d7bc2998db48a9fc506415ddc0438823b7f
tealPigeon <71701518+tealPigeon@users.noreply.github.com>
2021-07-28 04:45:55 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from 'react';
+import React from 'react';
 import Main_screen from './Main_screen/Main_screen';
 import UX_Principles from './UX_Principles/UX_Principles';
 
@@ -7,10 +7,9 @@ import  {Route, BrowserRouter} from "react-router-dom";
 const App = () =>{
     return (
         <BrowserRouter>
-            <Fragment>
                 <Route path="/" exact component={Main_screen}  />
+                <Route path="/summer_practice" exact component={Main_screen}  />
                 <Route path="/ux_principles" component={UX_Principles}  />
-            </Fragment>
         </BrowserRouter>
 
     );
